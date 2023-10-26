33d7c2b573bd69b3a8c9a93b6cbf5de6d6c6bb30
Stella <112723364+stella-craig@users.noreply.github.com>
2023-10-26 10:17:00 -0500

    Week 10 Notes

--- Notes/Week 10/server.js
+++ Notes/Week 10/server.js
@@ -10,5 +10,9 @@ app.get('/', (req, res) => res.render('index.ejs', {name: 'Scott'}))
 
 app.get('/login', (req, res) => res.render('login.ejs'))
 app.post('/login', (req, res) => {
-    
+
+})
+app.get('/register', (req, res) => res.render('register.ejs'))
+app.post('/register', (req, res) => {
+    req.body.email
 })
